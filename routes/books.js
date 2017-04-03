e72916794dcbc3cebc620443d65b1a274f43c386
garrick george <garrickgeorge@yahoo.com>
2017-04-02 20:00:12 -0600

    part 3 finish

--- routes/books.js
+++ routes/books.js
@@ -9,70 +9,66 @@ const humps = require('humps');
 const router = express.Router();
 
 router.get('/books', (req, res, next) => {
-    knex('books')
+  knex('books')
         .orderBy('title', 'asc')
         .then((books) => {
-            res.send(humps.camelizeKeys(books));
+          res.send(humps.camelizeKeys(books));
         })
         .catch((err) => {
-            next(err);
+          next(err);
         });
 });
 
 router.get('/books/:id', (req, res, next) => {
-    knex('books')
+  knex('books')
         .where('id', req.params.id)
         .then((books) => {
             // console.log('id', req.params.id)
-            res.send(humps.camelizeKeys(books[0]));
+          res.send(humps.camelizeKeys(books[0]));
         })
         .catch((err) => {
-            next(err);
+          next(err);
         });
 });
 router.post('/books', (req, res, next) => {
-    knex('books')
+  knex('books')
         .insert({
-            title: req.body.title,
-            author: req.body.author,
-            genre: req.body.genre,
-            description: req.body.description,
-            cover_url: req.body.coverUrl
+          title: req.body.title,
+          author: req.body.author,
+          genre: req.body.genre,
+          description: req.body.description,
+          cover_url: req.body.coverUrl
         })
         .returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
         .then((book) => {
-            res.send(humps.camelizeKeys(book[0]));
+          res.send(humps.camelizeKeys(book[0]));
         });
 });
 router.patch('/books/:id', (req, res, next) => {
-    knex('books')
+  knex('books')
         .where('id', req.params.id)
         .update({
-            title: req.body.title,
-            author: req.body.author,
-            genre: req.body.genre,
-            description: req.body.description,
-            cover_url: req.body.coverUrl
+          title: req.body.title,
+          author: req.body.author,
+          genre: req.body.genre,
+          description: req.body.description,
+          cover_url: req.body.coverUrl
         })
         .returning('*')
         .then((books) => {
-            res.json(humps.camelizeKeys(books[0]))
-        })
+          res.json(humps.camelizeKeys(books[0]));
+        });
 });
 router.delete('/books/:id', (req, res, next) => {
   let books;
 
-knex('books')
+  knex('books')
         .where('id', req.params.id)
         .returning('*')
         .del()
-        .then(books =>{
+        .then(books => {
           delete books[0].id;
           res.send(humps.camelizeKeys(books[0]));
-
-        })
-    
-
-
+        });
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,29 @@
 'use strict';
 
 const express = require('express');
-
+const bcrypt = require('bcrypt-as-promised')
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
-
+// YOUrouR CODE HERE
+route.post('/users', (req, res, next) => {
+    bcrypt.hash(req.body.password, 12)
+        .then((hashed_password) => {
+            console.log(req.body.email, hashed_password);
+            res.send(200);
+        })
+        return knex('users')
+        .insert({
+          email: req.body.email,
+          hashed_password: hashed_password
+        },'*')
+        .then((users) => {
+          user = users[0];
+          delete user.hashed_password;
+          res.send(user);
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
 module.exports = router;
